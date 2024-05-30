9f06d5197cf905e5d3615121004d9c3bf50fcaba
hassaammgl <h455a4mmehtab@gmail.com>
2024-05-30 15:33:22 +0500

    asf

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Button , Navbar , TextInput } from "flowbite-react";
-import { Link , useLocation } from "react-router-dom";
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
 
@@ -34,21 +34,23 @@ const Header = () => {
             <FaMoon />
           </Button>
           <Link to={"/sign-in"}>
-            <Button gradientDuoTone={"purpleToBlue"}>Sign In</Button>
+            <Button gradientDuoTone={"purpleToBlue"} outline>
+              Sign In
+            </Button>
           </Link>
           <Navbar.Toggle />
         </div>
-          <Navbar.Collapse>
-            <Navbar.Link active={path === "/"} as={"div"}>
-              <Link to={"/"}>Home</Link>
-            </Navbar.Link>
-            <Navbar.Link active={path === "/about"} as={"div"}>
-              <Link to={"/about"}>About</Link>
-            </Navbar.Link>
-            <Navbar.Link active={path === "/projects"} as={"div"}>
-              <Link to={"/projects"}>Projects</Link>
-            </Navbar.Link>
-          </Navbar.Collapse>
+        <Navbar.Collapse>
+          <Navbar.Link active={path === "/"} as={"div"}>
+            <Link to={"/"}>Home</Link>
+          </Navbar.Link>
+          <Navbar.Link active={path === "/about"} as={"div"}>
+            <Link to={"/about"}>About</Link>
+          </Navbar.Link>
+          <Navbar.Link active={path === "/projects"} as={"div"}>
+            <Link to={"/projects"}>Projects</Link>
+          </Navbar.Link>
+        </Navbar.Collapse>
       </Navbar>
     </>
   );
