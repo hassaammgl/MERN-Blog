ac2ccc3ac6fb44652216776b30d345d7fec9d9d9
hassaammgl <h455a4mmehtab@gmail.com>
2024-05-30 07:23:56 +0500

    All important folders are created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Dashboard from "./pages/Dashboard";
+import Signin from "./pages/Signin";
+import Singup from "./pages/Singup";
 
 const App = () => {
   return (
-    <div className='text-3xl text-red-500'>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Singup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App 
+export default App;
